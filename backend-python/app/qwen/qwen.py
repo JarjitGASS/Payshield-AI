c88997bf5f26fe1988dd0200d6d6d1e0ac61b558
Wenev <wenev.16@gmail.com>
2026-02-26 09:49:26 +0700

    feat: added qwen model description

--- backend-python/app/qwen/qwen.py
+++ backend-python/app/qwen/qwen.py
@@ -1,9 +1,21 @@
-# from openai import OpenAI
-# import os
+from openai import OpenAI
+import os
 
-# client = OpenAI(
-#     api_key = os.getenv("QWEN_API_KEY"),
-#     base_url=os.getenv("QWEN_BASE_URL", "https://dashscope.aliyuncs.com/compatible-mode/v1")
-# )
+client = OpenAI(
+    api_key = os.getenv("QWEN_API_KEY"),
+    base_url=os.getenv("QWEN_BASE_URL", "https://dashscope.aliyuncs.com/compatible-mode/v1")
+)
 
-# def qwen_chat(system_prompt: str, user_prompt: str):
+def qwen_chat(system_prompt: str, user_prompt: str, model: str = "qwen-max"):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ],
+        temperature=0.2,
+        top_p=0.8,
+        max_tokens=512,
+        response_format={"type": "json_object"}
+    )
+    return response.choices[0].message.content
