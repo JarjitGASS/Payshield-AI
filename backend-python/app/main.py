7b52ced6f7d61679714bdaf240248c60ac5cfde4
Shana Grace <shanagrace.sgs@gmail.com>
2026-02-26 21:55:33 +0700

    feat: add geoip

--- backend-python/app/main.py
+++ backend-python/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 import json
 import os
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from services.check_id_card import check_id_card
 from services.verivfy_id_card import verify_id_card
 from services.verify_email_age_card import verify_email_age
+from services.verify_geoip import check_geo_ip, get_real_ip
 
 load_dotenv()
 
@@ -47,3 +48,14 @@ async def verify_id_card_endpoint(file: UploadFile = File(...)):
 @app.post("/verify-email-age")
 async def verify_email_age_endpoint(email: str = Form()):
     return await verify_email_age(email)
+
+@app.post("/verify-geo-ip")
+async def verify_geo_ip_endpoint(
+    request: Request,
+    declared_country: str = Form(),
+    declared_city: str = Form(None)
+):
+
+    ip = get_real_ip(request)
+
+    return await check_geo_ip(ip, declared_country, declared_city)
