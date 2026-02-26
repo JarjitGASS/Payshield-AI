0ae7e081c9697921db726506573f1055509504a0
Wenev <wenev.16@gmail.com>
2026-02-26 08:34:57 +0700

    feat: dockerize backend frontend and db

---
+++ backend-python/app/qwen/qwen.py
@@ -0,0 +1,9 @@
+# from openai import OpenAI
+# import os
+
+# client = OpenAI(
+#     api_key = os.getenv("QWEN_API_KEY"),
+#     base_url=os.getenv("QWEN_BASE_URL", "https://dashscope.aliyuncs.com/compatible-mode/v1")
+# )
+
+# def qwen_chat(system_prompt: str, user_prompt: str):
